435a31e1363b7a151c843214b29550869477d29c
U-KOLIBRI\eugko <eugenekozlakov@gmail.com>
2021-02-24 19:14:14 -0500

    Fooled around with leddec amd tried out hexcounter lab.

--- Labs/leddec/leddec.srcs/sources_1/new/leddec.vhd
+++ Labs/leddec/leddec.srcs/sources_1/new/leddec.vhd
@@ -22,15 +22,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity leddec is
 	PORT (
 		dig : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
@@ -43,13 +34,20 @@ end leddec;
 architecture Behavioral of leddec is
 begin
 seg <= "0000001" WHEN data = "0000" ELSE --0
-	       "1001111" WHEN data = "0001" ELSE --1
-	       "0010010" WHEN data = "0010" ELSE --2
-	       "0000110" WHEN data = "0011" ELSE --3
-	       "1001100" WHEN data = "0100" ELSE --4
-	       "0100100" WHEN data = "0101" ELSE --5
-	       "0100000" WHEN data = "0110" ELSE --6
-	       "0001111" WHEN data = "0111" ELSE --7
+	       --"1001111" --(number 1)
+	       "0011000"WHEN data = "0001" ELSE --(letter P)
+	       --"0010010" number 2
+	       "0001100" WHEN data = "0010" ELSE  --letter q (it doesnt look that good)
+	       --"0000110" number 3
+	       "1000001" WHEN data = "0011" ELSE --letter "U"
+	       --"1001100" number 4
+	       "1001000" WHEN data = "0100" ELSE --letter "H"
+	       --"0100100"  number 5
+	       "0110110" WHEN data = "0101" ELSE --greek capital letter "Xi"
+	       --"0100000" number 6 
+	       "0111001" WHEN data = "0110" ELSE --greek capital letter gamma
+	       --"0001111" number 7
+	       "0001001" WHEN data = "0111" ELSE --greek letter capital pi
 	       "0000000" WHEN data = "1000" ELSE --8
 	       "0000100" WHEN data = "1001" ELSE --9
 	       "0001000" WHEN data = "1010" ELSE --A
@@ -60,7 +58,7 @@ seg <= "0000001" WHEN data = "0000" ELSE --0
 	       "0111000" WHEN data = "1111" ELSE --F
 	       "1111111";
 	-- Turn on anode of 7-segment display addressed by 2-bit digit selector dig
-	anode <= "1110" WHEN dig = "00" ELSE --0
+	anode <= "1110" WHEN dig = "00" ELSE --0 --Anode ) (U13?)
 	         "1101" WHEN dig = "01" ELSE --1
 	         "1011" WHEN dig = "10" ELSE --2
 	         "0111" WHEN dig = "11" ELSE --3
