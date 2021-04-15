efc792ebe0299eca7605a1d552890e462b78d1b3
U-KOLIBRI\eugko <eugenekozlakov@gmail.com>
2021-04-14 23:51:59 -0400

    Added BRAM IP core and attempted wiring together. Wiring error preventing completion

--- vivadoFiles/read_image_VHDL/read_image_VHDL.runs/impl_1/runme.sh
+++ vivadoFiles/read_image_VHDL/read_image_VHDL.runs/impl_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='D:/CPE487A-DSD/Labs/read_image_VHDL/read_image_VHDL.runs/impl_1'
+HD_PWD='D:/DSDFP/vivadoFiles/read_image_VHDL/read_image_VHDL.runs/impl_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .write_bitstream.begin.rst
+/bin/touch .init_design.begin.rst
 EAStep vivado -log read_image_VHDL.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source read_image_VHDL.tcl -notrace
 
 
